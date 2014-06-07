ac3f6c1677b4e0a26a520086c512c63c65c0d892
Sudip Guha <sudipguha29@gmail.com>
2014-06-07 06:40:34 -0700

    added more test code and edited the circuit file

--- circuit.py
+++ circuit.py
@@ -1,3 +1,6 @@
+"""
+This file contains most of the structures used to construct a circuit schematic
+"""
 class Circuit(object):
 	"""A Circuit representation"""
 
@@ -13,33 +16,36 @@ class Circuit(object):
 
 	def insert_components(self, component_name, number_of_pins, x_length, y_length):
 		"""Only method to be used to insert a component into the Circuit"""
-		self.components.append(component)
+		self.components.append(component_name, number_of_pins, x_length, y_length)
 
 
 class Component(object):
 	"""docstring for Component"""
-	def __init__(self, x_length, y_length, number_of_pins):
+	def __init__(self, component_name, number_of_pins, x_length, y_length):
 		super(Component, self).__init__()
 		self.x_length, self.y_length = x_length, y_length
 		self.number_of_pins = number_of_pins
+		self.pins = {}
 
-	def insert_connection(target, target_pin_number):
-		pass
-
-	#need to use dicts
+	def insert_connection(target, target_pin_number, source_pin_number):
+		
 
 
 class Connection(object):
-	"""docstring for Connection"""
-	def __init__(self, component1, component2):
+	"""representationo of a standard electrical connection that joins two elements"""
+	def __init__(self, component1, component1_pin_number, component2, component2_pin_number):
 		super(Connection, self).__init__()
 		self.component1 = component1
 		self.component2 = component2
+		self.component1_pin_number = component1_pin_number
+		self.component2_pin_number = component2_pin_number
 
 	@property
 	def end1(self):
+		"""returns the first connected component as a Component object"""
 		return self.component1
 
 	@property
 	def end2(self):
+		"""returns the second connected component as a Component object"""
 		return self.component2
---
+++ setup_test_circuit.py
@@ -0,0 +1,9 @@
+import circuit
+
+
+"""
+This file is meant serve as a testing ground and as a future reference for the correct implementation of the circuits
+defined in circuit.py 
+--June 7
+"""
+
