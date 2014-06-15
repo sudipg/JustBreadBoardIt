00f6d1ba1d7e75194466653cb784e0ea533b6815
Siddharth Iyer <sid8795@gmail.com>
2014-06-15 09:43:26 -0700

    Added Disjoint Sets

---
+++ disjoint_set.py
@@ -0,0 +1,45 @@
+"""
+disjoint_set.py
+"""
+
+class DisjointSet:
+  """
+  This is an ADT that implements a disjoint set when
+  each element is represented by a _unique_ number.
+  """
+  
+  def __init__(self, number_of_elements):
+    """ 
+    The constructor for this ADT.
+
+    number_of_elements -- The total number of elements the
+                          set is to have. 
+    """
+
+    self.array = [-1] * number_of_elements
+    
+  def union(self, root1, root2):
+    """ 
+    Unites two roots. In case the args are not roots, this
+    function calls self.find() and uses the roots of the numbers.
+    """
+
+    root1, root2 = self.find(root1), self.find(root2)
+    if (self.array[root2] < self.array[root1]):
+      self.array[root2] += self.array[root1]
+      self.array[root1] = root2
+    else:
+      self.array[root1] += self.array[root2]
+      self.array[root2] = root1
+
+  def find(self, int_x):
+    """ 
+    Finds the roots of the passed in number. If x is not a root,
+    it carries out path compression while looking for x's root.
+    """
+
+    if (self.array[int_x] < 0):
+      return int_x
+    else:
+      self.array[int_x] = self.find(self.array[int_x])
+      return self.array[int_x]
